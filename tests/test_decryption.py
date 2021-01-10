4a96a3d2ff5356d21499f33e57b6113b2ce8063f
Luis <luis.onofre@ciencias.unam.mx>
2021-01-10 15:13:19 -0600

    Add modularity

--- tests/test_decryption.py
+++ tests/test_decryption.py
@@ -1,22 +1,24 @@
 import pytest
-from secret_sharing.lagrange import interpolation, basis
+from secret_sharing.lagrange import Lagrange
+
+prime = 208351617316091241234326746312124448251235562226470491514186331217050270460481
 
 @pytest.mark.parametrize(["points", "single_point"],
 [
     ([(1, 13), (2, 38), (3, 93), (4, 190)], (0,6)),
     ([(3, 2151), (2, 1078), (-8, 9928), (11, 83743), (0, 0)], (1,433)),
     ([(23, 107032831),(9, 6459533),(16, 36105912), (7, 3049551)], (0, 8)),
-    ([(30, 103492800),(56, 2280380058),(73, 8516759882),
-    (-6, -19188),(23, 27899982), (35, 221794830)], (0, -30))
 ])
 def test_interpolation(points, single_point):
+    lagrange = Lagrange(prime)
     x, expected = single_point
-    assert interpolation(x, points) == expected
+    assert lagrange.interpolation(x, points) == expected
 
-@pytest.mark.parametrize(["points", "evaluation" ,"index", "expected"],
+@pytest.mark.parametrize(["points", "mod", "evaluation" ,"index", "expected"],
 [
-    ([(1,3),(2,7),(3,8)], 0, 1, -3),
-    ([(1,13),(2,38),(3,93),(4,190)], 0, 1, -6)
+    ([(1,3),(2,7),(3,8)], 11, 0, 1, 8),
+    ([(1,13),(2,38),(3,93),(4,190)], 13, 0, 0, 4)
 ])
-def test_basis(points, evaluation, index, expected):
-    assert basis(evaluation, points, index) == expected
+def test_basis(points, mod, evaluation, index, expected):
+    lagrange = Lagrange(mod)
+    assert (lagrange.basis(evaluation, points, index) % mod ) == expected
