c388a2d56630ff9201da8bd5709daa4fedc98489
Luis <luis.onofre@ciencias.unam.mx>
2021-01-17 11:50:39 -0600

    Add doc to decryption and lagrange

--- secret_sharing/decryption.py
+++ secret_sharing/decryption.py
@@ -5,15 +5,34 @@ from .lagrange import Lagrange
 prime = 208351617316091241234326746312124448251235562226470491514186331217050270460481
 
 def decrypt(file, points):
-    ''' Decrypt a file given a set of points '''
+    ''' Decrypt a file given a set of points
+    
+    Parameters
+    ----------
+    file: bytes
+        The file to decrypt
+    points:
+        A set of points (x,y) used as a key
 
-    lagrange = Lagrange(prime)
-    int_key = lagrange.interpolation(0, points)
+    Returns
+    -------
+    bytes
+        the file decrypted
+    '''
+    try:
+        lagrange = Lagrange(prime)
+        int_key = lagrange.interpolation(0, points)
+    except ArithmeticError as err:
+        raise
     
     key = int_key.to_bytes(32, byteorder='big')
     nonce = file[:AES.block_size]
     body = file[AES.block_size:]
-    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
-    decrypted_file = cipher.decrypt(body)
+    
+    try:
+        cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+        decrypted_file = cipher.decrypt(body)
+    except Exception as err:
+        raise
 
     return decrypted_file
--- secret_sharing/lagrange.py
+++ secret_sharing/lagrange.py
@@ -4,26 +4,59 @@ class Lagrange:
         self.prime = prime
 
     def interpolation(self, evaluation, points):
-        ''' Given a set of points, finds a polynomial that
-        passes through all those points and evaluates x'''
+        '''Given a set of points, finds a polynomial that
+        passes through all those points and evaluates x
+
+        Parameters
+        ----------
+        evaluation: int
+            The x-coordinate that will be evaluated
+        points:
+            A set of points to build the polynomial
+
+        Returns
+        -------
+        int
+            the result of the evaluation
+        '''
         result = 0
         index = 0
         for x,y in points:
+
             result += y * self.basis(evaluation, points, index)
             index += 1
         return result % self.prime 
 
     def basis(self, evaluation, points, current_index):
-        ''' Calculates the Lagrange basis'''
+        '''Calculates the Lagrange basis given an index
+        
+        Parameters
+        ----------
+        evaluation: int
+            The x-coordinate to evaluate 
+        points:
+            Set of points to calculate the basis
+        current_index: int
+            The index to find the current term to calculate
+            the basis
+
+        Returns
+        -------
+        int
+            the result of the basis at the given index
+        '''
         top = 1
         bottom = 1
         constant,_ = points[current_index]
         # Starts at -1 to initiate the loop at 0'
         i = -1 
-        for x,y in points:
+        for x,_ in points:
+            if not isinstance(x, int):
+                raise AritmeticError('Not an integer')
             i += 1
             if i == current_index:
                 continue
             top *= evaluation - x
             bottom *= constant - x
         return top * pow(bottom, -1, self.prime) 
+
